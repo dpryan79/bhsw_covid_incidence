c4b0ad8276f3ee84b9c5349fbb07734172bdae60
Devon Ryan <dpryan79@gmail.com>
2021-03-21 14:03:10 +0100

    Update scripts, add yaml and license

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 This repository is meant to compute and display the population-normalized Covid-19 incidence for each community (Gemeinde) in Breisgau-Hochschwarzwald per week. Only limited data is available, since it's [published weekly as a PDF](https://www.breisgau-hochschwarzwald.de/pb/Breisgau-Hochschwarzwald/Start/Service+_+Verwaltung/Corona-Virus.html) and previous week's data removed. GIS data is from [ESRI Deutschland](https://opendata-esri-de.opendata.arcgis.com/) and © GeoBasis-DE / BKG 2016.
 
 ![Animated map](https://github.com/dpryan79/bhsw_covid_incidence/raw/master/animation.gif)
+
+# Data harvesting
+
+Data is extracted from the PDF produced by the Landesregierun Breisgau-Hochscharzwald every week using pdftotext. Then the scripts under `scripts/` are used to format this into a standard TSV file (`incidence.txt`). The text file made from the PDF isn't always formatted exactly the same from week to week, so the python scripts used for this often need to be tweaked slightly.
+
+# Image creation
+
+The TSV data is loaded into R and the final animated GIF created using ggplot2, gganimate, sf and gifski.
--- scripts/makeAnimation.R
+++ scripts/makeAnimation.R
@@ -4,19 +4,28 @@ library(ggplot2)
 library(gganimate)
 library(gifski)
 library(transformr)  # not in conda-forge!
+
+# Load and subset GIS maps
 shps = st_read('GIS/GEM_2016.shp')
 bhsw = shps[which(shps$NUTS=='DE132'),]
-ggplot(bhsw) + geom_sf(aes(fill="GEN"))
+
+# Load incidence data, add color groups for ggplot2
 incidence = read.delim("incidence.txt")
 incidence$Date = as.Date(incidence$Date, format="%d.%m.%Y")
 breaks = c(0,25,35,50,100,200,350,500,1000,2000)
 breakLabels = sprintf(">=%i, <%i", breaks[1:length(breaks)-1], breaks[2:length(breaks)])
 incidence$Group = cut(incidence$Incidence,
                       breaks=breaks, labels=breakLabels, include.lowest=T)
+
+# Merge GIS and incidence
 bhswi = merge(bhsw, incidence, by.x="GEN", by.y="Location")
-colorscale=c("white", "#ffeda0", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a", "#e31a1c", "#bd0026", "#800026", "black")
+
+# Plot
+annotation = "GIS: © GeoBasis-DE / BKG 2016\nData: Landesregierung Breisgau-Hochschwarzwald\nImage: https://github.com/dpryan79/bhsw_covid_incidence"
+colorscale = c("white", "#ffeda0", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a", "#e31a1c", "#bd0026", "#800026", "black")
 g = ggplot(bhswi, aes(frame=Date)) + geom_sf(aes(fill=Group, group=seq_along(Group))) + theme_classic()
-g = g + labs(fill="Cases/100k\nper week") + scale_fill_manual(values=colorscale, guide = guide_legend(reverse=TRUE))
+g = g + annotate(geom="text", label=annotation, x=Inf, y=-Inf, hjust=1, vjust=0, size=3)
+g = g + labs(fill="Cases/100k\nper week", x="", y="") + scale_fill_manual(values=colorscale, guide = guide_legend(reverse=TRUE))
 g = g + transition_states(Date, state_length=3, transition_length=1) + ggtitle("Week of {closest_state}")
 g = g + theme(axis.line=element_blank(), axis.text=element_blank(), axis.ticks = element_blank())
 a = animate(g, renderer=gifski_renderer())
